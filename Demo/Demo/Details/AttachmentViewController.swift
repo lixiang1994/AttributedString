244281d96b3f89d0ee47db86643def38e1c9b1a9
Lee <18611401994@163.com>
2019-11-19 19:17:19 +0800

    .

---
+++ Demo/Demo/Details/AttachmentViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AttachmentViewController.swift
+//  Demo
+//
+//  Created by Lee on 2019/11/19.
+//  Copyright © 2019 LEE. All rights reserved.
+//
+
+import UIKit
+
+class AttachmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
